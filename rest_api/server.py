dc23ac189d267852f7415a6aae75f2a8e249e922
alicia639 <alicia.emberso.bremer@gmail.com>
2021-09-19 00:22:13 -0400

    fixing rest api

--- rest_api/server.py
+++ rest_api/server.py
@@ -7,17 +7,26 @@ import numpy as np
 from PIL import Image
 import numpy as np
 from skimage import transform
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt\
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import base64
 
 app = Flask(__name__)
 api = Api(app)
 
 class Photo(Resource):
-    def get(self, file_path):
-        
+
+    def get(self, json_image):
+
+        img_string = json_image["img"]
+
+        imgdata = base64.b64decode(str(img_string))
+
         model = load_model('./best_model.h5')
 
-        np_image = Image.open(filename)
+        np_image = Image.open(io.BytesIO(imgdata))
         np_image = np.array(np_image).astype('float32')/255
 
         im_h, im_w = np_image.shape[:2]
@@ -38,13 +47,11 @@ class Photo(Resource):
             if result == 0 and pred > 0.5:
                 #print(np.average(np.mean(img, axis=(0, 1))))
                 if (np.average(np.mean(img, axis=(0, 1)))) <  0.3:
-                    return 0
+                    return {"val": 0}
 
-        return 1
-        
+        return {"val": 1}
 
 api.add_resource(Photo, '/photos') # Route_1
-
 
 if __name__ == '__main__':
      app.run(port='5002')
